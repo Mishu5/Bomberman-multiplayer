74156140d5a6246d708dd6b50ca958bf5aa29129
Mishu5 <michalgostkow@gmail.com>
2023-05-21 22:37:18 +0200

    change map logic

--- Server/src/main/java/Shared/Map.java
+++ Server/src/main/java/Shared/Map.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 
 public class Map {
 
-    ArrayList<MapObject> destructibleWalls = new ArrayList<MapObject>();
-    ArrayList<MapObject> indestructibleWalls = new ArrayList<MapObject>();
-    ArrayList<MapObject> powerUPs = new ArrayList<MapObject>();
-    ArrayList<MapObject> floor = new ArrayList<MapObject>();
-    ArrayList<MapObject> players = new ArrayList<MapObject>();
-    ArrayList<MapObject> bombs = new ArrayList<MapObject>();
+    ArrayList<MapObject> map = new ArrayList<MapObject>();
 
     public void addDestructibleWall(int positionX, int positionY) {
 
