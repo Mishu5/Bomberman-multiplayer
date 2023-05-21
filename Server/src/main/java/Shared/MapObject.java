160b0b551f6531666c9303c6d14c145bca109d73
Mishu5 <michalgostkow@gmail.com>
2023-05-21 22:41:23 +0200

    add new variables to MapObject

--- Server/src/main/java/Shared/MapObject.java
+++ Server/src/main/java/Shared/MapObject.java
@@ -2,6 +2,10 @@ package Shared;
 
 public class MapObject {
 
+    private boolean destructible;
+
+    private boolean transparent;
+
     private int positionX;
     private int positionY;
 
@@ -21,9 +25,11 @@ public class MapObject {
         this.positionY = positionY;
     }
 
-    public MapObject(int positionX, int positionY) {
+    public MapObject(int positionX, int positionY, boolean destructible, boolean transparent) {
         this.positionX = positionX;
         this.positionY = positionY;
+        this.destructible = destructible;
+        this.transparent = transparent;
     }
 
 }
