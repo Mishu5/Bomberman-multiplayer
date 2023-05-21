eac7b79bc878e94f20651e1bf70c145cbe095f6d
Mishu5 <michalgostkow@gmail.com>
2023-05-21 21:55:28 +0200

    add getter and setter to MapObject

--- Server/src/main/java/Shared/MapObject.java
+++ Server/src/main/java/Shared/MapObject.java
@@ -2,4 +2,22 @@ package Shared;
 
 public class MapObject {
 
+    private int positionX;
+    private int positionY;
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
 }
