c031a2fc15df6242f9638c4599f24758616203f2
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-05-22 19:17:31 +0200

    Fix gradle files.

--- desktop/src/com/bomberman/desktop/DesktopLauncher.java
+++ desktop/src/com/bomberman/desktop/DesktopLauncher.java
@@ -4,7 +4,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.bomberman.client.Bomberman;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/**
+ * Please note that on macOS your application needs to
+ * be started with the -XstartOnFirstThread JVM argument
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
