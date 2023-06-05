1fab0e1499abe4fcb0c47f6626f4bbbc73a2d42b
Mishu5 <michalgostkow@gmail.com>
2023-06-05 21:55:37 +0200

    add ClientConnectionHandler and ClientHandler

---
+++ common/src/com/bomberman/common/engine/ClientHandler.java
@@ -0,0 +1,5 @@
+package com.bomberman.common.engine;
+
+public class ClientHandler {
+
+}
---
+++ server/src/com/bomberman/server/ClientConnectionHandler.java
@@ -0,0 +1,7 @@
+package com.bomberman.server;
+
+import java.io.*;
+import java.net.*;
+
+public class ClientConnectionHandler extends Thread{
+}
