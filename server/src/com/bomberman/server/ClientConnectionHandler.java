e77d8de2cf6257b2abb29a55c32d5a181f86f874
Mishu5 <michalgostkow@gmail.com>
2023-06-05 23:01:53 +0200

    add input to ClientHandlerThread

--- server/src/com/bomberman/server/ClientConnectionHandler.java
+++ server/src/com/bomberman/server/ClientConnectionHandler.java
@@ -10,64 +10,95 @@ import static java.lang.System.exit;
 import java.io.*;
 import java.net.*;
 
-public class ClientConnectionHandler extends Thread{
+public class ClientConnectionHandler extends Thread {
 
     private GameServices gameEngine;
     private final int MAX_PLAYER_COUNT = 4;
     private final int SOCKET_INITIALIZATION_ERROR = 1;
     private final int SOCKET_CLIENT_ACCEPTANCE_ERROR = 2;
+    private final int OUTPUT_STREAM_CREATION_ERROR = 3;
+    private final int BUFFERED_READER_CREATION_ERROR = 4;
 
     //server socket related
     private final int PORT = 21370;
     private ServerSocket serverSocket;
     private Socket clientSocket;
 
-    public ClientConnectionHandler(GameServices gameEngine){
+    public ClientConnectionHandler(GameServices gameEngine) {
         this.gameEngine = gameEngine;
     }
 
-    public void run(){
+    public void run() {
 
         initializeSockets();
 
         System.out.println("Waiting for players...");
 
-        while(true){
+        while (true) {
 
             acceptPlayer();
 
             //checking for available slots
             int currentPlayerCount = gameEngine.getMap().getPlayers().size();
-            if(currentPlayerCount == MAX_PLAYER_COUNT)continue;
+            if (currentPlayerCount == MAX_PLAYER_COUNT) continue;
 
             //adding new player
             /**
              * TODO
              * Player constructor should define player position
              */
-            PlayerHandler currentPlayerHandler = gameEngine.addPlayer(new Player(1,1,currentPlayerCount));
+            BufferedReader tempBufferedReaderHolder = makeBufferedReader(clientSocket);
+            PlayerHandler currentPlayerHandler = gameEngine.addPlayer(new Player(1, 1, currentPlayerCount));
 
         }
 
     }
 
-    private void initializeSockets(){
-        try{
+    private void initializeSockets() {
+        try {
             serverSocket = new ServerSocket(PORT);
-        }catch (IOException e){
+        } catch (IOException e) {
             exit(SOCKET_INITIALIZATION_ERROR);
         }
     }
 
-    private void acceptPlayer(){
+    private void acceptPlayer() {
 
-        try{
+        try {
             clientSocket = serverSocket.accept();
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Failed to accept client");
             exit(SOCKET_CLIENT_ACCEPTANCE_ERROR);
         }
 
+    }
+
+    private ObjectOutputStream makeNewObjectOutPutStream(Socket playerSocket) {
+
+        ObjectOutputStream tempObjectOutputStreamHolder = null;
+
+        try {
+            tempObjectOutputStreamHolder = new ObjectOutputStream(playerSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Error in creating output stream");
+            exit(OUTPUT_STREAM_CREATION_ERROR);
+        }
+        return tempObjectOutputStreamHolder;
+
+    }
+
+    private BufferedReader makeBufferedReader(Socket clientSocket) {
+
+        BufferedReader tempBufferedReaderHolder = null;
+
+        try {
+            tempBufferedReaderHolder = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("Error in creating buffered reader");
+            exit(BUFFERED_READER_CREATION_ERROR);
+        }
+
+        return tempBufferedReaderHolder;
     }
 
 }
--- server/src/com/bomberman/server/ClientHandlerThread.java
+++ server/src/com/bomberman/server/ClientHandlerThread.java
@@ -1,14 +1,29 @@
 package com.bomberman.server;
 
 import com.bomberman.common.engine.PlayerHandler;
+import java.io.*;
+import java.net.*;
 
 public class ClientHandlerThread extends Thread{
 
     private final PlayerHandler playerHandler;
+    private final BufferedReader in;
 
-    public ClientHandlerThread(PlayerHandler playerHandler){
+    public ClientHandlerThread(PlayerHandler playerHandler, BufferedReader in){
         this.playerHandler = playerHandler;
+        this.in = in;
     }
+
+    synchronized public void run(){
+
+        System.out.println("CLient " + playerHandler.getID() + " listener");
+
+        while(true){
+
+        }
+
+    }
+
 
 
 }
