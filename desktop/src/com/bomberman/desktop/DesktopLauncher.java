577f7ab49bb36dfbfc48a31cfcc4a27bba0f1d2b
Mishu5 <michalgostkow@gmail.com>
2023-06-09 17:21:23 +0200

    Rebase config.txt

--- desktop/src/com/bomberman/desktop/DesktopLauncher.java
+++ desktop/src/com/bomberman/desktop/DesktopLauncher.java
@@ -6,12 +6,22 @@ import com.bomberman.client.Bomberman;
 
 import static com.bomberman.common.utils.GraphicUtils.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+
 /**
  * Please note that on macOS your application needs to
  * be started with the -XstartOnFirstThread JVM argument
  */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
+		//getting ip
+		String ip = getIp("config.txt");
+		System.out.println(ip);
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(FPS);
 		config.setTitle("Bomberman");
@@ -22,8 +32,29 @@ public class DesktopLauncher {
 				WINDOW_HEIGHT
 		);
 
-		System.out.println("TEST");
-
 		new Lwjgl3Application(new Bomberman(), config);
 	}
+
+	private static String getIp(String filename){
+
+		String ip = null;
+		File config;
+		Scanner file;
+
+		try{
+
+			config = new File(filename);
+			file = new Scanner(config);
+
+			ip = file.nextLine();
+
+			file.close();
+		}catch (FileNotFoundException e){
+			System.out.println("No file");
+			return null;
+		}
+
+		return ip;
+	}
+
 }
