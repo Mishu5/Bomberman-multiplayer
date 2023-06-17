5b0d5bcd5883836cbe046b4b26eee3daee21d3c4
Mishu5 <michalgostkow@gmail.com>
2023-06-17 15:30:36 +0200

    add cool down logic to Player class

--- common/src/com/bomberman/common/model/Player.java
+++ common/src/com/bomberman/common/model/Player.java
@@ -8,6 +8,9 @@ public class Player extends MapObject {
 
     private int playerID;
 
+    private int movementCoolDown;
+    private int bombCoolDown;
+
     public Player(int positionX, int positionY, int playerID) {
         super(positionX, positionY, true, true, PLAYER_TEXTURE);
         this.playerID = playerID;
@@ -20,6 +23,44 @@ public class Player extends MapObject {
     public void move(int x, int y) {
         positionX = x;
         positionY = y;
+    }
+
+    public boolean movementCoolDownPassed() {
+        return movementCoolDown == 0;
+    }
+
+    public boolean bombCoolDownPassed() {
+        return bombCoolDown == 0;
+    }
+
+    public void reduceMovementCoolDown(int val) {
+        movementCoolDown -= val;
+        if (movementCoolDown < 0) {
+            movementCoolDown = 0;
+        }
+    }
+
+    public void reduceBombCoolDown(int val) {
+        bombCoolDown -= val;
+        if (bombCoolDown < 0) {
+            bombCoolDown = 0;
+        }
+    }
+
+    public void setMovementCoolDown(int coolDown) {
+        movementCoolDown = coolDown;
+    }
+
+    public void setBombCoolDown(int coolDown) {
+        bombCoolDown = coolDown;
+    }
+
+    public int getMovementCoolDown() {
+        return movementCoolDown;
+    }
+
+    public int getBombCoolDown() {
+        return bombCoolDown;
     }
 
 }
